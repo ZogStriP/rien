cf2a1aa27f0998dd8c9e5c98065d1d792b8627d5
Régis Hanol <regis@hanol.fr>
2025-09-01 15:04:38 +0200

    Update config.nix

--- config.nix
+++ config.nix
@@ -10,6 +10,79 @@ in {
     hm.nixosModules.home-manager
   ];
 
+  disko.devices = {
+    disk.nvme = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          esp = {
+            type = "EF00";
+            size = "512M";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
+            };
+          };
+          luks = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "luks";
+              settings.allowDiscards = true;
+              settings.bypassWorkqueues = true;
+              content = {
+                type = "btrfs";
+                subvolumes = {
+                  "@nix" = { 
+                    mountpoint = "/nix"; 
+                    mountOptions = [ "noatime" ];
+                  };
+                  "@persist" = { 
+                    mountpoint = "/persist"; 
+                    mountOptions = [ "noatime" ];
+                  };
+                  "@log" = { 
+                    mountpoint = "/var/log"; 
+                    mountOptions = [ "noatime" ];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    nodev = {
+      "/" = { 
+        fsType = "tmpfs"; 
+        mountOptions = [ "size=128M" "mode=0755" ]; 
+      };
+      "/tmp" = { 
+        fsType = "tmpfs"; 
+        mountOptions = [ "size=4G" "mode=1777" ]; 
+      };
+    };
+  };
+
+  zramSwap.enable = true;
+
+  environment.persistence."/persist" = {
+    hideMounts = true;
+
+    files = [
+      "/etc/machine-id"
+    ];
+
+    directories = [
+      "/var/lib/nixos"
+      "/var/lib/systemd"
+    ];
+  };
+
   nixpkgs.hostPlatform = "x86_64-linux";
 
   system.stateVersion = stateVersion;
